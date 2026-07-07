7a8bc6b94ccebaeaf4bed9ca63be748a069f0278
Victor Muller <victormuller050@gmail.com>
2026-07-07 19:16:24 -0300

    Alterado texto de minha historia e ocultado sessões nao usadas por enquanto

--- js/main.js
+++ js/main.js
@@ -5,7 +5,12 @@ document.addEventListener('DOMContentLoaded', async () => {
   const loaded = await carregarBioLink();
   if (!loaded) return;
 
-  VideoPlayer.init();
-  Gallery.init();
+  if (!document.getElementById('video')?.hidden) {
+    VideoPlayer.init();
+  }
+
+  if (!document.getElementById('galeria')?.hidden) {
+    Gallery.init();
+  }
   Animations.init();
 });
